ec59dba9462eec47fb8d9e2f5237f4be1706dc23
fejjari-oussama <oussamafejjari69@gmail.com>
2020-10-16 03:10:52 +0100

    goodnight<3

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,12 +1,56 @@
  import React from 'react'
+ 
+ class Menu extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+          ordredFood:[],
+          total:0
+ 
+        }
+        
+    }
+    checkboxes(){
+      var con = document.getElementById('container');
+      var items = con.getElementsByTagName('input');
+      var arr=[]
+      var cost=0
+      
+      for(var i=0;i<items.length;i++){
+        // console.log(items[i].name)
+        // console.log(items[i].value)
+        if(items[i].checked){
+          arr.push(items[i].name)
+          cost+= JSON.parse(items[i].value)
+    }
+   
+    }
+    this.setState({ordredFood:arr,total:cost})
 
-const Menu = ({food,price}) => (
+  }
+   
+   
+  
+     render(){
+      console.log(this.state)
+         return(
+          
+         <div id="container" >
+                 {this.props.menu.map(item => (
+                     <div key={item.id}>
+          <h4  >{item.food} {item.price}</h4>
+           <input type="checkbox" value={item.price} name={item.food}></input>
+           
+          </div>
 
-    <div>
-        <div>{food}</div>
-        <div>{price}</div>
-    </div>
-
-
-)
+        ))}
+              <div>
+                  <button onClick={this.checkboxes.bind(this) }>submit</button>
+            </div>
+         </div>
+         
+               )
+             }
+ 
+ }
 export default Menu;
